a16e6d287174e763fa371b6d1d2fd4338d1d80b5
fjguarde <programador0705@gmail.com>
2021-09-08 21:31:56 +0200

    http methods to edit, delete, get heroes

--- src/app/services/heroes.service.ts
+++ src/app/services/heroes.service.ts
@@ -1,5 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { filter } from 'rxjs/operators';
+import { Hero } from '../models/interfaces';
 
 @Injectable({
   providedIn: 'root'
@@ -11,4 +13,20 @@ export class HeroesService {
   public getHeroes(url: string){
     return this.httpClient.get(url);
   }
+
+  public getHeroeById(urlParam: string, id: string){
+      return this.httpClient.get(urlParam)
+      .pipe(
+        filter( (hero: any)=> hero.id === id)
+        );
+  }
+
+  public updateHero(id: string){
+    
+  }
+
+  public deleteHero(id: string){
+
+  }
+
 }
