b527234b054c089dcfdcc54cfd5966653031da7f
fjguarde <programador0705@gmail.com>
2021-09-13 17:24:41 +0200

    Add translates

--- src/app/components/confirm-dialog/confirm-dialog.module.ts
+++ src/app/components/confirm-dialog/confirm-dialog.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core'
 import { CommonModule } from '@angular/common'
 import { MatDialogModule } from '@angular/material/dialog'
 import { ConfirmDialogComponent } from './confirm-dialog.component'
+import { TranslateModule } from '@ngx-translate/core'
 
 
 @NgModule({
   declarations: [ConfirmDialogComponent],
   imports: [
     CommonModule,
-    MatDialogModule
+    MatDialogModule,
+    TranslateModule
   ]
 })
 export class ConfirmDialogModule { }
