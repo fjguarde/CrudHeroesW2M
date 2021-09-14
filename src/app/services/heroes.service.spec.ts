8eac1d256dae79dbf4256a404f1d7e9fe0be82d2
fjguarde <programador0705@gmail.com>
2021-09-14 23:26:28 +0200

    coverage improvements on unit test

--- src/app/services/heroes.service.spec.ts
+++ src/app/services/heroes.service.spec.ts
@@ -1,14 +1,14 @@
-import { HttpClientModule } from '@angular/common/http'
 import { TestBed } from '@angular/core/testing'
-
 import { HeroesService } from './heroes.service'
 import { Hero } from '../models/interfaces'
-import { Observable } from 'rxjs'
+import { HttpTestingController, HttpClientTestingModule } from '@angular/common/http/testing'
+import { environment } from 'src/environments/environment'
 
 
 describe('HeroesService', () => {
   let service: HeroesService
-  const heroesMock: Hero = {
+  let httpMock : HttpTestingController
+  const heroMock: Hero = {
     id: '0',
     name: 'Batman', 
     publisher: 'DC Comics', 
@@ -20,13 +20,42 @@ describe('HeroesService', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
       imports: [
-        HttpClientModule
+        HttpClientTestingModule
       ]
     })
     service = TestBed.inject(HeroesService)
+    httpMock = TestBed.inject(HttpTestingController)
   })
 
   it('should be created', () => {
     expect(service).toBeTruthy()
+  })
+
+  it('getHeroes return a list of heroes and does a get method', () => {
+    service.getHeroes().subscribe((resp: Hero[]) => {
+        expect(resp).toEqual([heroMock])
+    })
+    const req = httpMock.expectOne(environment.apiUrl + '/heroes')
+    expect(req.request.method).toBe('GET')
+    req.flush([heroMock])
+  })
+
+  it('updateHeroes return a list of heroes and does a post method', () => {
+    service.updateHero(heroMock).subscribe((resp: Hero) => {
+        expect(resp).toEqual(heroMock)
+    })
+    const req = httpMock.expectOne(environment.apiUrl + '/heroes')
+    expect(req.request.method).toBe('POST')
+    req.flush(heroMock)
+  })
+
+  it('deleteHero return id of deleted and does a delete method', () => {
+    const id = '1'
+    service.deleteHero(id).subscribe((resp: string) => {
+        expect(resp).toEqual(id)
+    })
+    const req = httpMock.expectOne(environment.apiUrl + `/heroes?id=${id}`)
+    expect(req.request.method).toBe('DELETE')
+    req.flush(id)
   })
 })
